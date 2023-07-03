1cbe6ed877b62fd5aa8cf3777313503483dd717e
STARFAILING <138183812+STARFAILING@users.noreply.github.com>
2023-07-03 16:54:19 +0800

    新分支

--- src/main/java/com/weng/config/WebMvcConfig.java
+++ src/main/java/com/weng/config/WebMvcConfig.java
@@ -1,24 +1,24 @@
-package com.weng.config;
-
-import com.weng.common.util.JacksonObjectMapper;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer
-{
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters)
-    {
-        //创建消息转换器
-        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter=new MappingJackson2HttpMessageConverter();
-        //设置对象转换器，底层使用Jackson将Java转换为json
-        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
-        //将上面的消息转换器对象追加到mvc框架的转换器集合中
-        converters.add(0,mappingJackson2HttpMessageConverter);
-    }
-}
+//package com.weng.config;
+//
+//import com.weng.common.util.JacksonObjectMapper;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.List;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer
+//{
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters)
+//    {
+//        //创建消息转换器
+//        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter=new MappingJackson2HttpMessageConverter();
+//        //设置对象转换器，底层使用Jackson将Java转换为json
+//        mappingJackson2HttpMessageConverter.setObjectMapper(new JacksonObjectMapper());
+//        //将上面的消息转换器对象追加到mvc框架的转换器集合中
+//        converters.add(0,mappingJackson2HttpMessageConverter);
+//    }
+//}
